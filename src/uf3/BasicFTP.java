3769b67fae348a788d7db1d37128c87fa56142ec
Asg <formu@ASUS-LAPTOP>
2019-12-08 23:32:18 +0100

    1.3 Modificacions en pujaFitxer

--- src/uf3/BasicFTP.java
+++ src/uf3/BasicFTP.java
@@ -1,11 +1,9 @@
 package uf3;
 
-import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
-import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 
@@ -51,20 +49,21 @@ public class BasicFTP {
 			}
 
 			String comanda = scan.next();
-			
+
 			if (comanda.equals("mkdir")) {
 				String directori = scan.next();
 				creaDirectori(directori);
 			} else if (comanda.equals("delete")) {
 				String fitxer = scan.next();
 				esborraFitxer(fitxer);
-			} else if (comanda.equals("put")) {
-				String fitxer = scan.next();
-				pujaFitxer(fitxer, ServerFTP, usuari, contrasenya);
-			}  else if (comanda.equals("rename")) {
+			} else if (comanda.equals("rename")) {
 				String nom = scan.next();
 				String nom2 = scan.next();
 				renombraFitxer(nom, nom2);
+			} else if (comanda.equals("put")) {
+				String directoriRemot = scan.next();
+				FileInputStream directoriLocal = new FileInputStream(directoriRemot);
+				pujaFitxer(directoriRemot, directoriLocal);
 			}
 
 		} catch (IOException ioe) {
@@ -99,46 +98,25 @@ public class BasicFTP {
 			ioe.printStackTrace();
 		}
 	}
-	
-	public static void pujaFitxer(String adreca, String Servidor, String usuari, String contrasenya) {
-		try {
-			System.out.println("Ens connectem al servidor: "+Servidor);
-			client.connect(Servidor);
-			boolean login = client.login(usuari, contrasenya);
-			
-			if (login) {
 
-				client.changeWorkingDirectory(adreca);
-				client.setFileType(FTP.BINARY_FILE_TYPE);
-				
-				//Stream d'entrada amb el fitxer que es vol pujar
-				BufferedInputStream in = new BufferedInputStream(new FileInputStream("/home/user/text1.txt"));
-				client.storeFile("text1.txt", in);
-				
-				//Stream d'entrada amb el fitxer que es vol pujar
-				in = new BufferedInputStream(new FileInputStream("/home/user/world01.png"));
-				client.storeFile("world01.png", in);
-				
-				in.close();
-				client.logout();
-				client.disconnect();
-				System.out.println("L'operació ha acabat... ");
-				
-			}
-			
-		} catch (IOException ioe) {
-			
-			ioe.printStackTrace();
-			
-		}
-	}
-	
 	public static void renombraFitxer(String nom, String nom2) {
 		try {
 			if (client.rename(nom, nom2)) {
 				System.out.println("Fitxer renombrat... ");
 			} else {
 				System.out.println("No s'ha pogut renombrar el fitxer... ");
+			}
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	public static void pujaFitxer(String directoriRemot, FileInputStream directoriLocal) {
+		try {
+			if (client.storeFile(directoriRemot, directoriLocal)) {
+				System.out.println("Fitxer pujat... ");
+			} else {
+				System.out.println("No s'ha pogut pujar el fitxer... ");
 			}
 		} catch (IOException ioe) {
 			ioe.printStackTrace();
